8e01b677f2338e1d70dcb16cbd2a1f8c4a85ecd6
CSBCD-SOEST <cdintodb@gmail.com>
2016-12-22 17:34:26 -1000

    Added database schema

--- app/schema.sql
+++ app/schema.sql
@@ -1,6 +1,28 @@
-drop table if exists questions;
-create table questions (
-	id integer primary key autoincrement,
-	question text not null,
-	answer text not null
+drop table if exists question;
+create table question (
+	questionid 	integer primary key autoincrement,
+	questiontext 	text not null
 );
+
+drop table if exists response;
+create table response (
+	responseid 	integer primary key autoincrement,
+	responsetime	integer not null,
+	response 	integer not null,
+	questionid	integer not null,
+	optionid	integer not null,
+	FOREIGN KEY(questionid) REFERENCES question(questionid),
+	FOREIGN KEY(optionid) REFERENCES response(responseid)
+);
+
+drop table if exists option;
+create table option (
+	optionid	integer primary key autoincrement,
+	optiontext	text not null,
+	questionid	integer not null,
+	FOREIGN KEY(questionid) REFERENCES question(questionid)
+);
+	
+
+
+	 
